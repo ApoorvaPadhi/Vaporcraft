a2293df9a595eb22336965feeec5ca2f4cdc4571
ApoorvaPadhi <apoorvapadhi2000@gmail.com>
2024-12-07 22:22:40 -0600

    minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,8 @@ export default function Home() {
         initial={{ opacity: 0, y: 50 }}
         animate={inView ? { opacity: 1, y: 0 } : {}}
         transition={{ duration: 1 }}
-        {...props}
+        className={className}
+        style={style}
       >
         {children}
       </motion.section>
